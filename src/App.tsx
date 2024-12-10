20a0a1aa8f524e11b2b533af5802c5a9019e619e
rotimiWaris <warisrotimi@gmail.com>
2024-12-09 23:15:02 -0500

    Fixed redireting issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import supabase from "./config/supabaseClient";
 import Home from "./components/Home";
